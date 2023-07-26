73199ecbdcf1fe1b75254a2d6ea24a358ecca382
Fer <ferp@protonmail.ch>
2023-07-26 14:35:26 +0200

    fix element positioning

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Centro León Hebreo',
-  description: 'Página web del Centro León Hebreo',
-}
+  title: "Centro León Hebreo",
+  description: "Página web del Centro León Hebreo",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
