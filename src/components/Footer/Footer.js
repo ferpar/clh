4e6dcfa84979633ed181f015db0fce3789084b9b
Fer <ferp@protonmail.ch>
2023-07-26 18:08:43 +0200

    further work on header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,21 @@
+import styles from './Footer.module.css';
+
 function Footer() {
   return (
-    <footer>
-      <p>En construcción...</p>
+    <footer className={styles['site-footer']}>
+      <nav>
+        <ul className={styles['footer-nav-links']}>
+            <li className={styles['footer-nav-link']}>
+                <a  href="/">Aviso Legal</a>
+            </li>
+            <li className={styles['footer-nav-link']}>
+                <a href="/cursos">Política de Privacidad</a>
+            </li>
+            <li className={styles['footer-nav-link']}>
+                <a href="/equipo">Configuración de Cookies</a>
+            </li>
+        </ul>
+      </nav>
     </footer>
   );
 }
