0fe8a86cd69960d52391ce58c89ae45b55441058
Fer <ferp@protonmail.ch>
2023-07-29 21:32:26 +0200

    Header improvements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,8 @@
-'use client'
+"use client";
 import React from "react";
 import Link from "next/link";
 import styles from "./Header.module.css";
 import { getString } from "../../helpers/i18n";
-
 
 function Header() {
   const [scrolled, setScrolled] = React.useState(null);
@@ -14,30 +13,38 @@ function Header() {
       setScrolled(false);
     }
   };
-  React.useEffect( () => {
+  React.useEffect(() => {
     window.addEventListener("scroll", checkScroll);
     return () => {
       window.removeEventListener("scroll", checkScroll);
-    }; 
+    };
   }, []);
   return (
-    <header className={`${styles["site-header"]} ${ scrolled && styles['scrolled']}`}>
-      <a>
-        <h1>CLH</h1>
-      </a>
-      <nav>
-        <ul className={styles["header-nav-links"]}>
-          <li className={styles["header-nav-link"]}>
-            <Link href="/">{getString("HOME")}</Link>
-          </li>
-          <li className={styles["header-nav-link"]}>
-            <Link href="/cursos">{getString("COURSES")}</Link>
-          </li>
-          <li className={styles["header-nav-link"]}>
-            <Link href="/equipo">{getString("TEAM")}</Link>
-          </li>
-        </ul>
-      </nav>
+    <header
+      className={`${styles["site-header"]}`}
+    >
+      <div className={`${styles["wrapper"]} ${scrolled && styles["scrolled"]}`}>
+        <a>
+          <img
+            src="/images/logo.webp"
+            alt="CLH"
+            className={`${styles["logo"]} ${scrolled && styles["logo-scrolled"]}`}
+          />
+        </a>
+        <nav>
+          <ul className={styles["header-nav-links"]}>
+            <li className={styles["header-nav-link"]}>
+              <Link href="/">{getString("HOME")}</Link>
+            </li>
+            <li className={styles["header-nav-link"]}>
+              <Link href="/cursos">{getString("COURSES")}</Link>
+            </li>
+            <li className={styles["header-nav-link"]}>
+              <Link href="/equipo">{getString("TEAM")}</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
