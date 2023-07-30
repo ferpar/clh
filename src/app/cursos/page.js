44703e63311c2e0a4e2fec2abdcc709b382361da
Fer <ferp@protonmail.ch>
2023-07-30 19:29:50 +0200

    CourseList iteration 1

--- src/app/cursos/page.js
+++ src/app/cursos/page.js
@@ -1,29 +1,12 @@
 import styles from "./page.module.css";
-import { getString } from "../../helpers/i18n";
 import Highlighted from "@/components/Highlighted";
+import CourseList from "@/components/Courses";
 
 export default function Courses() {
   return (
     <main className={styles.main}>
       <Highlighted />
-      <section className={styles["courses"]}>
-        <h1>{getString("TITLE")}</h1>
-        <ul>
-          <li>
-            <img src="/images/lighthouse2.webp" alt="lighthouse" />
-            <h3>{getString("LIGHTHOUSE")}</h3>
-            <p>
-              ¿Te animas a conocer con nosotros la historia y la cultura del
-              pueblo judío de Sefarad?
-            </p>
-            <p>
-              Te ayudamos a acceder al conocimiento de una manera amena e
-              intuitiva, mediante el enfoque del diálogo, siguiendo la didáctica
-              establecida desde la antigüedad, y por el propio León Hebreo.
-            </p>
-          </li>
-        </ul>
-      </section>
+      <CourseList /> 
     </main>
   );
 }
