1717a554796812e27b329a9c0972447c8df05382
Fer <ferp@protonmail.ch>
2023-08-04 21:30:53 +0200

    basic use for seeing the bio

--- src/app/equipo/page.js
+++ src/app/equipo/page.js
@@ -1,5 +1,6 @@
 import members from './members.js';
 import styles from './page.module.css';
+import TeamCard from '../../components/TeamCard';
 
 export default function Team() {
     return (
@@ -7,18 +8,7 @@ export default function Team() {
             <h2 className={styles["title"]}>Nuestro Equipo</h2>
            <div className={styles["container"]}> 
                 {members.map((member, index) => (
-                    <div className={styles["card"]} key={index}>
-                        <div className={styles["card-img"]}>
-                            <img src={member.img} alt={member.name} />
-                        </div>
-                        <div className={styles["card-info"]}>
-                            <h3>{member.name} {member.surname}</h3>
-                            <h4>{member.role1} {member.role2}</h4>
-                            <hr/>
-                            <p>{member.tagline}</p>
-                            {/* <p>{member.bio}</p> */}
-                        </div>
-                    </div>
+                    <TeamCard member={member} key={index} />
                 ))}
            </div>
         </section>
