62dcb3e67628938dfd1bdf10716d3edd7b8cde5c
Fer <ferp@protonmail.ch>
2023-08-04 11:00:37 +0200

    Refacto FadeIn, added extend parameter to control height above the fold for 100% visibility, added fade in effect to course card images.

--- src/components/FadeIn/FadeIn.js
+++ src/components/FadeIn/FadeIn.js
@@ -3,19 +3,23 @@ import React from "react"
 import styles from "./FadeIn.module.css"
 
 // component that fades in children as they are scrolled into view
-function FadeIn({ children, ...delegated}) {
+// extend controls how high above the fold the children are 100% visible
+// if extend is 2, 
+// then the children will be 100% visible when the domRef is 200% above the fold
+
+function FadeIn({ extend = 1, children, ...delegated}) {
     const [opacity, setOpacity] = React.useState(1) // ratio between 0 and 1
     const domRef = React.useRef()
     const handleScroll = () => {
         if (!domRef.current) return
-        const { top } = domRef.current.getBoundingClientRect()
+        const { top, height } = domRef.current.getBoundingClientRect()
         const windowHeight = window.innerHeight
         // proportion of the domRef that is visible
-        const visibleProportion =  top / windowHeight
+        const visibleProportion =  ((windowHeight - top) / height) / extend
         const opacityRatio = () => {
-            if (visibleProportion > 1) return 0
-            if (visibleProportion < 0.2) return 1
-            return 1 - visibleProportion
+            if (visibleProportion > 1) return 1
+            if (visibleProportion < 0) return 0
+            return visibleProportion
         }
         setOpacity(opacityRatio())
     }
