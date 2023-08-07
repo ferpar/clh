bc9487834a2271dc266481b6b1568ff1a657a3df
Fer <ferp@protonmail.ch>
2023-08-07 14:49:54 +0200

    Footer Links now update state variable for theming the Header

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,46 @@
-import styles from './Footer.module.css';
-import Link from 'next/link'
-import {getString} from '../../helpers/i18n'
+"use client";
+import React from "react";
+import { NavContext } from "../NavContext";
+import styles from "./Footer.module.css";
+import Link from "next/link";
+import { getString } from "../../helpers/i18n";
+
+const footerLinks = [
+  {
+    name: "LEGAL",
+    path: "/aviso-legal",
+  },
+  {
+    name: "PRIVACY",
+    path: "/politica-de-privacidad",
+  },
+  {
+    name: "COOKIES",
+    path: "/configuracion-de-cookies",
+  },
+];
 
 function Footer() {
+  const { setCurrentPath } = React.useContext(NavContext);
+
+  const handlePathChange = (newURL) => {
+    setCurrentPath(newURL);
+  };
+
   return (
-    <footer className={styles['site-footer']}>
+    <footer className={styles["site-footer"]}>
       <nav>
-        <ul className={styles['footer-nav-links']}>
-            <li className={styles['footer-nav-link']}>
-                <Link  href="/aviso-legal">{getString('LEGAL')}</Link>
+        <ul className={styles["footer-nav-links"]}>
+          {footerLinks.map((link) => (
+            <li key={link.name} className={`${styles["footer-nav-link"]}`}>
+              <Link
+                href={link.path}
+                onClick={() => handlePathChange(link.path)}
+              >
+                {getString(link.name)}
+              </Link>
             </li>
-            <li className={styles['footer-nav-link']}>
-                <Link href="/politica-de-privacidad">{getString('PRIVACY')}</Link>
-            </li>
-            <li className={styles['footer-nav-link']}>
-                <Link href="/configuracion-de-cookies">{getString('COOKIES')}</Link>
-            </li>
+          ))}
         </ul>
       </nav>
     </footer>
