c3c2d87a438e4e7337813ee1cfc796097be61868
Fer <ferp@protonmail.ch>
2023-08-27 18:16:27 +0200

    added alternative header for vercel deployments

--- src/components/Header/WrappedHeader.js
+++ src/components/Header/WrappedHeader.js
@@ -5,7 +5,7 @@ import styles from './Header.module.css'
 // wrapping the header in a server component to get the url from request headers
 export default async function WrappedHeader({ ...delegated }) {
     const headerList = headers()
-    const path = headerList.get('x-invoke-path') || null
+    const path = headerList.get('x-invoke-path') || headerList.get('x-matched-path') || null
     return (
         <header className={styles["site-header"]}>
             <Header path={path} {...delegated}/>
